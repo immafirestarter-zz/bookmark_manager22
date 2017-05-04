7f7355bbf3e156f79d3c25b3e353aae814b2f3d6
Sami <sami@Samis-MacBook-Pro.local>
2017-05-04 13:37:57 +0100

    add extra s to 'Johns'

--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -2,10 +2,10 @@ feature 'it allows us to tag links' do
   it 'assigns tags' do
     visit '/link/new'
     fill_in 'title', with: 'John'
-    fill_in 'tag', with: 'Johns'
+    fill_in 'tag', with: 'Johnss'
     fill_in 'url', with: 'John.com'
     click_button 'Submit!'
      link = Link.first
-     expect(link.tags.map(&:name)).to include('Johns')
+     expect(link.tags.map(&:name)).to include('Johnss')
   end
 end
